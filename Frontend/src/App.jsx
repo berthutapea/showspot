b66a45ba382ac5fd754aa05e596feaf5f82f9a68
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-05-30 01:58:51 +0700

    feature: landinge page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../public/vite.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import Login from './pages/login';
+import NotFound from './pages/notfound';
+import Navbar from './components/navbar';
+import Footer from './components/footer';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </Router>
   );
-}
+};
 
 export default App;
--- Frontend/src/components/buttons/secondary-button/index.jsx
+++ Frontend/src/components/buttons/secondary-button/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SecondaryButton = ({ children, className, onClick }) => {
   return (
     <button
-      className={`${className} inline-flex items-center justify-center gap-3 rounded-md bg-meta-3 py-2 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10`}
+      className={`${className} btn text-black flex items-center gap-2 border-2 border-primary bg-transparent hover:bg-primary hover:border-transparent hover:text-white duration-500`}
       onClick={onClick}
     >
       {children}
