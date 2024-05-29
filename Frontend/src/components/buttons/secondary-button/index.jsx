080ce031192fa61d0634158be241b653ba316409
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-05-29 22:02:35 +0700

    refactor: secondarybutton

--- Frontend/src/components/buttons/secondary-button/index.jsx
+++ Frontend/src/components/buttons/secondary-button/index.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-export default function SecondaryButton() {
+const SecondaryButton = ({ children, className, onClick }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <button
+      className={`${className} inline-flex items-center justify-center gap-2 rounded-md bg-meta-3 py-2 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default SecondaryButton;
