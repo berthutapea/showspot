a95f682d463f7d5431d2f6be15a443ecd4bb821c
ramdanialdi889@gmail.com <ramdanialdi899@gmail.com>
2024-05-31 10:15:38 +0700

    feature: Delete Data User

--- Backend/core/Model.js
+++ Backend/core/Model.js
@@ -20,7 +20,8 @@ class Model {
       this.database.closeConnection();
       return results;
     } catch (error) {
-
+      console.error('Error create data from the database: ', error);
+      return false;
     }
   }
 
@@ -34,7 +35,71 @@ class Model {
       return results;
     } catch (error) {
       console.error('Error fetching data from the database: ', error);
-      throw error;
+      return false;
+    }
+  }
+
+  async findOne(datas) {
+    try {
+      await this.database.openConnection();
+      const field = Object.keys(datas);
+      const value = Object.values(datas);
+
+      let query = `SELECT * FROM ${this.tableName} WHERE ${field[0]} = ?`;
+      const [results] = await this.database.connection.query(query, value);
+      this.database.closeConnection();
+      return results;
+    } catch (error) {
+      console.error('Error fetching data from the database: ', error);
+      return false;
+    }
+  }
+
+  async update(params, datas) {
+    try {
+      await this.database.openConnection();
+      const param = Object.keys(params);
+      const paramValue = Object.values(params);
+      const fieldsArg = Object.keys(datas);
+      const newValues = Object.values(datas);
+
+      const setStatement = fieldsArg
+        .map((field, index) => `${field} = '${newValues[index]}'`)
+        .join(', ');
+
+      const whereClause = param.map(
+        (param, index) => `${param} = '${paramValue[index]}'`
+      );
+
+      const query = `UPDATE ${this.tableName} SET ${setStatement} WHERE ${whereClause}`;
+
+      const result = await this.database.connection.query(query);
+      this.database.closeConnection();
+
+      return result;
+    } catch (error) {
+      console.error('Error from the database: ', error);
+      return false;
+    }
+  }
+
+  async delete(params) {
+    try {
+      await this.database.openConnection();
+      const param = Object.keys(params);
+      const paramValue = Object.values(params);
+
+      const setStatement = param.map((field, index) => `${field} = '${paramValue[index]}'`);
+
+      const query = `DELETE FROM ${this.tableName} WHERE ${setStatement}`;
+
+      const result = await this.database.connection.query(query);
+      this.database.closeConnection();
+
+      return result;
+    } catch (error) {
+      console.error('Error from the databse: ', error);
+      return false;
     }
   }
 }
