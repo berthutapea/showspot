04f409eaf6e166d5ca159a6553450a6eba3e22b2
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-06-10 21:15:10 +0700

    feature: feat: save-as-pdf

--- Frontend/src/pages/dashboard/admin/projects/sop-projects/index.jsx
+++ Frontend/src/pages/dashboard/admin/projects/sop-projects/index.jsx
@@ -1,14 +1,24 @@
-// src/pages/sop/SopProjects.js
 import React from 'react';
 import Layout from '../../../../../layout';
 import Breadcrumb from '../../../../../components/breadcrumb';
 import SOP from '../../../../../components/sop';
+import { TfiPrinter } from 'react-icons/tfi';
+import OneButton from '../../../../../components/buttons/one-button';
+import { Link } from 'react-router-dom';
 
 const SopProjects = () => {
   return (
     <Layout>
       <Breadcrumb pageName="Sop Projects" />
-      <div className="sm:grid-cols-2">
+      <Link to="/admin/sop-projects">
+        <OneButton>
+          <span>Save as PDF</span>
+          <span>
+            <TfiPrinter />
+          </span>
+        </OneButton>
+      </Link>
+      <div className="sm:grid-cols-2 mt-6">
         <div className="flex flex-col gap-9">
           <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
             <div className="border-b border-stroke py-4 px-6.5 dark:border-strokedark">
