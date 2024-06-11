cb217c6f25c6789f0d63db861d10ae4abc03aec5
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-06-11 20:54:17 +0700

    feat: addbuttons

--- Frontend/src/pages/dashboard/admin/projects/sop-projects/index.jsx
+++ Frontend/src/pages/dashboard/admin/projects/sop-projects/index.jsx
@@ -5,19 +5,44 @@ import SOP from '../../../../../components/sop';
 import { TfiPrinter } from 'react-icons/tfi';
 import OneButton from '../../../../../components/buttons/one-button';
 import { Link } from 'react-router-dom';
+import TwoButton from '../../../../../components/buttons/two-button';
+import FourButton from '../../../../../components/buttons/four-button';
+import { FaEdit, FaPlus } from 'react-icons/fa';
 
 const SopProjects = () => {
   return (
     <Layout>
       <Breadcrumb pageName="Sop Projects" />
-      <Link to="/admin/sop-projects">
-        <OneButton>
-          <span>Save as PDF</span>
-          <span>
-            <TfiPrinter />
-          </span>
-        </OneButton>
-      </Link>
+      <div className="flex flex-col md:flex-row w-full gap-3 text-center py-4">
+        <div className="flex gap-3">
+          <Link to="/admin/sop-projects">
+            <OneButton>
+              <span>Add Sop</span>
+              <span>
+                <FaPlus />
+              </span>
+            </OneButton>
+          </Link>
+          <Link to="/admin/sop-projects">
+            <FourButton>
+              <span>Edit Sop</span>
+              <span>
+                <FaEdit />
+              </span>
+            </FourButton>
+          </Link>
+        </div>
+        <div className="ml-auto">
+          <Link to="/admin/sop-projects">
+            <TwoButton>
+              <span>Save as PDF</span>
+              <span>
+                <TfiPrinter />
+              </span>
+            </TwoButton>
+          </Link>
+        </div>
+      </div>
       <div className="sm:grid-cols-2 mt-6">
         <div className="flex flex-col gap-9">
           <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
