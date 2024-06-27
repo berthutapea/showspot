a188d5c2fa6b2ea2f24bf76aba82c0146274c517
msnproduction <111676859+berthutapea@users.noreply.github.com>
2024-06-27 13:15:32 +0700

    fix: auth

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,10 +5,6 @@ import store from './configs/redux/store.js';
 import App from './App.jsx';
 import './index.css';
 
-import axios from 'axios';
-
-axios.defaults.withCredentials = true;
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
