409e285b1b525fc4e703efa33624f9e4f1765388
Aldi Ramdani <ramdanialdi889@gmail.com>
2024-11-02 10:14:56 +0700

    fix login & logout handler

--- Backend/services/HashingService.js
+++ Backend/services/HashingService.js
@@ -3,7 +3,12 @@ const argon2 = require('argon2');
 class HashingService {
   async generateHash(data) {
     try {
-      const hash = await argon2.hash(data);
+        const hash = await argon2.hash(data, {
+          type: argon2.argon2id,
+          memoryCost: 2 ** 16,
+          timeCost: 3,
+          parallelism: 1
+      });
       return hash;
     } catch (error) {
       console.error('Error generating hash:', error);
@@ -13,6 +18,7 @@ class HashingService {
 
   async compareHash(data, hash) {
     try {
+      console.log(await argon2.verify(hash, data));
       const result = await argon2.verify(hash, data);
       return result;
     } catch (error) {
@@ -22,4 +28,7 @@ class HashingService {
   }
 }
 
+const instance = new HashingService();
+// instance.generateHash('admin');
+// instance.compareHash('admin', '$argon2id$v=19$m=65536,t=3,p=4$w3gJEmQxpalgsCRebRW4QA$argon2id$v=19$m=65536,t=3,p=4$1ryi2ny+Ss7nL7c9eLLuuw$YPr74kGg/a8c0PDMp2LfaYCg1ySGlEpvZPqkqYxSvDE');
 module.exports = { HashingService };
